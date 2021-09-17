7b193605c1fdc6c56c2042f890053c44efda144a
Ahmad abobakr <AhmadaboBakr@gmail.com>
2021-09-17 22:34:07 +0400

    removed Network System

--- NetworkSystem/Intrerfaces/IServer.cs
+++
@@ -1,9 +0,0 @@
-﻿
-public interface IServer 
-{
-    void Send(Message message,uint reciverID);
-    void Broadcast(Message message);
-    void Join(string id);
-    void Create(string id);
-    void Update();
-}
